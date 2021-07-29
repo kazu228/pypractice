0aaa9b39876b2c09eda83af4ce5ca976451071ab
kazuaki <kskkm022@gmail.com>
2021-07-29 20:56:46 +0900

    線を引く

--- grasample.py
+++ grasample.py
@@ -20,16 +20,28 @@ def draw_ball():
     cv.create_oval(ball["x"] - ball["z"], ball["y"] - ball["z"],
                     ball["x"] + ball["z"], ball["y"] + ball["z"], fill="red")
 
+
+def draw_line():
+    x1 = 0
+    x2 = 600
+    y = 210
+    cv.create_line(x1, y, x2, y)
+
+
 def move_ball(e):
     
     if e.keysym == "Right":
         ball["x"] += ball["dirx"]
 
     if e.keysym == "Left":
-        ball["x"] -= ball["diry"]
+        ball["x"] -= ball["dirx"]
+
+    if e.keysym == "Up":
+        ball["y"] -= ball["diry"]
 
 def main():
     draw_ball()
+    draw_line()
     win.bind("<KeyPress>", move_ball)
     win.after(50, main)
 
